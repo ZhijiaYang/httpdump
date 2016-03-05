56000368d8677dbac8e067e47a4cb66e0e856bcf
zhijia <yangzhijia_1@163.com>
2016-03-06 00:03:57 +0800

    update httpdump

--- httpdump.go
+++ httpdump.go
@@ -5,20 +5,11 @@ import (
 	"flag"
 	"fmt"
 	"os"
-
+	"strings"
 	"github.com/akrennmair/gopcap"
 )
 
-const (
-	TYPE_IP  = 0x0800
-	TYPE_ARP = 0x0806
-	TYPE_IP6 = 0x86DD
 
-	IP_ICMP = 1
-	IP_INIP = 4
-	IP_TCP  = 6
-	IP_UDP  = 17
-)
 
 var out *bufio.Writer
 var errout *bufio.Writer
@@ -26,14 +17,13 @@ var errout *bufio.Writer
 func main() {
 	var device *string = flag.String("i", "", "interface")
 	var snaplen *int = flag.Int("s", 65535, "snaplen")
-	var hexdump *bool = flag.Bool("X", false, "hexdump")
 	expr := ""
 
 	out = bufio.NewWriter(os.Stdout)
 	errout = bufio.NewWriter(os.Stderr)
 
 	flag.Usage = func() {
-		fmt.Fprintf(errout, "usage: %s [ -i interface ] [ -s snaplen ] [ -X ] [ expression ]\n", os.Args[0])
+		fmt.Fprintf(errout, "usage: %s [ -i interface ] [ -s snaplen ] [ expression ]\n", os.Args[0])
 		errout.Flush()
 		os.Exit(1)
 	}
@@ -47,7 +37,7 @@ func main() {
 	if *device == "" {
 		devs, err := pcap.Findalldevs()
 		if err != nil {
-			fmt.Fprintf(errout, "tcpdump: couldn't find any devices: %s\n", err)
+			fmt.Fprintf(errout, "httpdump: couldn't find any devices: %s\n", err)
 		}
 		if 0 == len(devs) {
 			flag.Usage()
@@ -57,7 +47,7 @@ func main() {
 
 	h, err := pcap.Openlive(*device, int32(*snaplen), true, 0)
 	if h == nil {
-		fmt.Fprintf(errout, "tcpdump: %s\n", err)
+		fmt.Fprintf(errout, "httpdump: %s\n", err)
 		errout.Flush()
 		return
 	}
@@ -66,56 +56,38 @@ func main() {
 	if expr != "" {
 		ferr := h.Setfilter(expr)
 		if ferr != nil {
-			fmt.Fprintf(out, "tcpdump: %s\n", ferr)
+			fmt.Fprintf(out, "httpdump: %s\n", ferr)
 			out.Flush()
 		}
 	}
 
 	for pkt := h.Next(); pkt != nil; pkt = h.Next() {
 		pkt.Decode()
-		fmt.Fprintf(out, "%s\n", pkt.String())
-		if *hexdump {
-			Hexdump(pkt)
-		}
+		HttpRequest(pkt)
+	}
+}
+
+func HttpRequest(pkt *pcap.Packet) {
+	if len(pkt.Payload) < 4 {
+		return
+	}
+	head := string(pkt.Payload[:4])
+	if head[:3] == "GET" || head == "POST" {
+		out.WriteString("[*] " +  pkt.String())
+		out.WriteString("\n[*] Request:\n" +  string(pkt.Payload))
 		out.Flush()
 	}
+	if head == "HTTP" {
+		head = string(pkt.Payload)
+		index := strings.Index(head, "\r\n\r\n")
+		if index > 0 {
+			out.WriteString("[*] " +  pkt.String())
+			out.WriteString("\n[*] Response:\n" +  head[:index+4])
+			out.Flush()
+		}
+
+	}
+
 }
 
-func min(a, b int) int {
-	if a < b {
-		return a
-	}
-	return b
-}
 
-func Hexdump(pkt *pcap.Packet) {
-	for i := 0; i < len(pkt.Data); i += 16 {
-		Dumpline(uint32(i), pkt.Data[i:min(i+16, len(pkt.Data))])
-	}
-}
-
-func Dumpline(addr uint32, line []byte) {
-	fmt.Fprintf(out, "\t0x%04x: ", int32(addr))
-	var i uint16
-	for i = 0; i < 16 && i < uint16(len(line)); i++ {
-		if i%2 == 0 {
-			out.WriteString(" ")
-		}
-		fmt.Fprintf(out, "%02x", line[i])
-	}
-	for j := i; j <= 16; j++ {
-		if j%2 == 0 {
-			out.WriteString(" ")
-		}
-		out.WriteString("  ")
-	}
-	out.WriteString("  ")
-	for i = 0; i < 16 && i < uint16(len(line)); i++ {
-		if line[i] >= 32 && line[i] <= 126 {
-			fmt.Fprintf(out, "%c", line[i])
-		} else {
-			out.WriteString(".")
-		}
-	}
-	out.WriteString("\n")
-}
